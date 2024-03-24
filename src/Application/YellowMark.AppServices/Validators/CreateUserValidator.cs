a3e560af03e0f77da998b6251ccc06800d40826b
CabageMan <1iron.head1@gmail.com>
2024-03-24 21:43:38 +0300

    Update phone number validation

--- src/Application/YellowMark.AppServices/Validators/CreateUserValidator.cs
+++ src/Application/YellowMark.AppServices/Validators/CreateUserValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
 using YellowMark.Contracts;
 
 namespace YellowMark.AppServices.Validators;
@@ -21,18 +22,33 @@ public class CreateUserValidator : AbstractValidator<CreateUserRequest>
             .MaximumLength(30);
 
         RuleFor(user => user.Email)
-            .EmailAddress();
+            .NotEmpty()
+            .NotNull()
+            .EmailAddress()
+            .WithMessage("A valid email address is required.");
 
-        // TODO: Create validation for phone number.
+        int minPhoneLength = 11;
+        int maxPhoneLength = 15;
+        // TODO: Update phone regex if it needed. Check in w.
+        // Phone format: 1(234)567-8901
         RuleFor(user => user.Phone)
             .NotEmpty()
-            .NotNull();
+            .NotNull()
+            .MinimumLength(minPhoneLength)
+            .WithMessage($"The phone number must be at least {minPhoneLength} characters.")
+            .MaximumLength(maxPhoneLength)
+            .WithMessage($"The phonae number must not exceed {maxPhoneLength} characters.")
+            .Matches(new Regex(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}"))
+            .WithMessage("PhoneNumber not valid");
+
+
+
 
         var eighteenYearsAgo = DateOnly.FromDateTime(DateTime.Now.AddYears(-18));
         RuleFor(user => user.BirthDate)
-        .NotEmpty()
-        .NotNull()
-        .LessThanOrEqualTo(eighteenYearsAgo)
-        .WithMessage("You must be at least 18 years old.");
+            .NotEmpty()
+            .NotNull()
+            .LessThanOrEqualTo(eighteenYearsAgo)
+            .WithMessage("You must be at least 18 years old.");
     }
 }
